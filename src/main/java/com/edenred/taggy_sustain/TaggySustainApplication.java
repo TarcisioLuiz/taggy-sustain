e835fdf3e87360bfbe8761985d26babf59376395
TarcisioLuiz <tarcisioluizfilho@gmail.com>
2026-04-17 23:08:47 -0300

    Estrutura base do projeto

---
+++ src/main/java/com/edenred/taggy_sustain/TaggySustainApplication.java
@@ -0,0 +1,13 @@
+package com.edenred.taggy_sustain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaggySustainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaggySustainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edenred/taggy_sustain/TaggySustainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edenred.taggy_sustain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaggySustainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
