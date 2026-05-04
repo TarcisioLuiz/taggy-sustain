64beb62a35ddf0e0ee65aa9933b16656eb85a20f
André Montenegro <126609983+andre4383@users.noreply.github.com>
2026-05-04 15:05:59 -0300

    Login

--- src/main/java/com/edenred/taggy_sustain/controller/UsuarioController.java
+++ src/main/java/com/edenred/taggy_sustain/controller/UsuarioController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/usuario")
+@RequestMapping("/api/v1/usuario")
 public class UsuarioController {
 
     @Autowired
