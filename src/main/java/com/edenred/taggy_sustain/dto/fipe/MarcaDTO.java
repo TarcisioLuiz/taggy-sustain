44f6974e1d93d6e30b8ad1bca978c14c4cce02a6
André Montenegro <126609983+andre4383@users.noreply.github.com>
2026-05-28 23:36:18 -0300

    Implementação brasilAPI, tabela FIPE

---
+++ src/main/java/com/edenred/taggy_sustain/dto/fipe/MarcaDTO.java
@@ -0,0 +1,9 @@
+package com.edenred.taggy_sustain.dto.fipe;
+
+import lombok.Data;
+
+@Data
+public class MarcaDTO {
+    private String nome;
+    private String valor; // Corresponde ao código da marca na API
+}
